aac213b2140cf75173da2d4cbabcf178721eb05c
Mihajlo Simevski <msimevskii@gmail.com>
2023-09-23 04:55:56 +0200

    feat: added subject template with time and cover
    Added a subject template that contains a time on top and a cover image with a change button

--- src/components/Layout/ContentLayout/ContentLayout.tsx
+++ src/components/Layout/ContentLayout/ContentLayout.tsx
@@ -1,10 +1,12 @@
 import SubjectCreator from '../../SubjectCreator/SubjectCreator';
+import SubjectTemplate from '../../SubjectTemplate/SubjectTemplate';
 import './ContentLayout.scss';
 
 export default function ContentLayout() {
   return (
     <main className="ContentLayout">
       <SubjectCreator />
+      <SubjectTemplate />
     </main>
   );
 }
---
+++ src/components/SubjectTemplate/SubjectTemplate.tsx
@@ -0,0 +1,23 @@
+import ChangeIcon from '../../assets/icons/change-icon.svg';
+import Button from '../Button/Button';
+
+import './SubjectTemplate.scss';
+
+export default function SubjectTemplate() {
+  return (
+    <section className="SubjectTemplate">
+      <div className="subject-wrapper">
+        <p className="subject-time">
+          <span className="subject-theme-circle" />
+          8:30 - 10:15
+        </p>
+        <section className="subject-cover">
+          <Button variant="primary" type="button">
+            <img src={ChangeIcon} alt="change cover icon" />
+            Change Subject Image
+          </Button>
+        </section>
+      </div>
+    </section>
+  );
+}
