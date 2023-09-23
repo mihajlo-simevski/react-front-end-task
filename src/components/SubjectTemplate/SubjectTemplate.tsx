3fb0ebc76cc6d0ae544c720b26433005c84a14fb
Mihajlo Simevski <msimevskii@gmail.com>
2023-09-24 00:54:15 +0200

    feat: added a subject header section

--- src/components/SubjectTemplate/SubjectTemplate.tsx
+++ src/components/SubjectTemplate/SubjectTemplate.tsx
@@ -1,4 +1,7 @@
 import ChangeIcon from '../../assets/icons/change-icon.svg';
+import EditIcon from '../../assets/icons/edit-icon.svg';
+import OptionIcon from '../../assets/icons/option-icon.svg';
+import RectangleIcon from '../../assets/icons/rectangle-icon.svg';
 import Button from '../Button/Button';
 import SwitchField from '../SwitchField/SwitchField';
 
@@ -8,17 +11,34 @@ export default function SubjectTemplate() {
   return (
     <section className="SubjectTemplate">
       <div className="subject-wrapper">
-        <p className="subject-time">
-          <span className="subject-theme-circle" />
-          8:30 - 10:15
-        </p>
-        <section className="subject-cover">
-          <Button variant="primary" type="button">
-            <img src={ChangeIcon} alt="change cover icon" />
-            Change Subject Image
-          </Button>
-        </section>
-        <SwitchField />
+        <div>
+          <p className="subject-time">
+            <span className="subject-theme-circle" />
+            8:30 - 10:15
+          </p>
+          <section className="subject-cover">
+            <Button variant="primary" type="button">
+              <img src={ChangeIcon} alt="change cover icon" />
+              Change Subject Image
+            </Button>
+          </section>
+          <SwitchField />
+        </div>
+      </div>
+      <div className="content-wrapper">
+        <div className="subject-title">
+          <figure>
+            <figcaption>Subject title</figcaption>
+            <div>
+              <img src={RectangleIcon} alt="rectangle icon" />
+              <h1>Some subject title</h1>
+              <img src={EditIcon} alt="edit icon" />
+            </div>
+          </figure>
+          <div className="option">
+            <img src={OptionIcon} alt="rectangle icon" />
+          </div>
+        </div>
       </div>
     </section>
   );
